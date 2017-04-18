e510c51db3804c1624a4098b6ad24cd8faa9892c
NinaDesgranges <n.desgranges@reply.it>
2017-04-18 18:34:18 -0400

    'try'

--- app.py
+++ app.py
@@ -51,21 +51,21 @@ def index():
         js_resources = INLINE.render_js()
         css_resources = INLINE.render_css()
 
-        # script, div = da.templateUsMapPercAcceptedLoan()
+        script, div = da.templateUsMapPercAcceptedLoan()
 
-        # html = render_template(
-        #     'new_index_map.html',
-        #     plot_script=script,
-        #     plot_div=div,
-        #     js_resources=js_resources,
-        #     css_resources=css_resources
-        # )
-
-        return render_template(
-            'ciao.html',
+        html = render_template(
+            'new_index.html',
+            plot_script=script,
+            plot_div=div,
             js_resources=js_resources,
             css_resources=css_resources
         )
+
+        # return render_template(
+        #     'ciao.html',
+        #     js_resources=js_resources,
+        #     css_resources=css_resources
+        # )
 
         # return render_template(
         #     'new_index_map.html',
@@ -75,7 +75,7 @@ def index():
         #     css_resources=css_resources
         # )
 
-        #return encode_utf8(html)
+        return encode_utf8(html)
 
         # return render_template('loan_perc_states_map.html')
         # return render_template('index.html',
